48f4116204d6832b235804abeb2cd263034dfbd8
Wu-Haitao <59434361+Wu-Haitao@users.noreply.github.com>
2020-08-17 12:09:32 +0800

    Add Increment: Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
 public class Duke {
     public static void main(String[] args) {
+        showLogo();
+        drawHorizontalLine(40);
+        greet();
+        drawHorizontalLine(40);
+        exit();
+        drawHorizontalLine(40);
+    }
+
+    private static void showLogo(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    private static void drawHorizontalLine(int length) {
+        String horizontalLine = "";
+        for (int i = 0; i < length; i++) horizontalLine += '-';
+        System.out.println(horizontalLine);
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
