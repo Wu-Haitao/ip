39d62de5d8b3027fa19edaa25efef57e01832190
Wu-Haitao <59434361+Wu-Haitao@users.noreply.github.com>
2020-08-22 11:23:48 +0800

    Add Increment: Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,50 @@
-public class Duke {
+import java.util.Scanner;
+
+class Duke {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String command = "";
+
+        drawHorizontalLine(40);
         showLogo();
         drawHorizontalLine(40);
         greet();
         drawHorizontalLine(40);
-        exit();
+
+        do {
+            command = scan.nextLine();
+            echo(command);
+        } while (!command.equals("bye"));
+
+    }
+
+    private static void echo(String command) {
+        switch (command) {
+        case "list":
+            list();
+            break;
+        case "joke":
+            joke();
+            break;
+        case "bye":
+            exit();
+            break;
+        default:
+            System.out.println("Invalid command.");
+            break;
+        }
         drawHorizontalLine(40);
     }
 
-    private static void showLogo(){
+    private static void list() {
+        System.out.println("List");
+    }
+
+    private static void joke() {
+        System.out.println("There are only 10 kinds of people in this world:\nthose who know binary and those who don’t.");
+    }
+
+    private static void showLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -24,7 +60,7 @@ public class Duke {
     }
 
     private static void greet() {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke.\nWhat can I do for you?");
     }
 
     private static void exit() {
